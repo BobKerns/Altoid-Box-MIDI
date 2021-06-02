f87c303d2abd6f56c4cbcdb4c414d68d3ed2e06d
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2021-06-01 20:06:30 -0700

    Expose the item count of menus.

--- lib/Menu/Menu.h
+++ lib/Menu/Menu.h
@@ -4,27 +4,29 @@
 
 template<class T>
 class Menu {
-  const uint8_t count;
-  const char * const * items;
-  uint8_t selection;
-  bool wrap_items = false;;
-  static const uint8_t top = 0;
-  static const uint8_t left = 0;
-  static const uint8_t width = 128;
-  static const uint8_t height = 64 - top;
-  public:
-    Menu(uint8_t count, const char * const *items);
-    Menu(uint8_t count, uint8_t selection, const char* const*items);
-    Menu &select(uint8_t i);
-    void draw(T &display);
-    inline Menu &wrap(bool wrapItems = true) {
-      wrap_items = wrapItems;
-      return *this;
-    }
-    inline uint8_t size() const {
-      return count;
-    }
-    inline const char *item(uint8_t idx) const {
-      return items[idx];
-    }
+    public:
+        const uint8_t count;
+    private:
+        const char * const * items;
+        uint8_t selection;
+        bool wrap_items = false;;
+        static const uint8_t top = 0;
+        static const uint8_t left = 0;
+        static const uint8_t width = 128;
+        static const uint8_t height = 64 - top;
+    public:
+        Menu(uint8_t count, const char * const *items);
+        Menu(uint8_t count, uint8_t selection, const char* const*items);
+        Menu &select(uint8_t i);
+        void draw(T &display);
+        inline Menu &wrap(bool wrapItems = true) {
+            wrap_items = wrapItems;
+            return *this;
+        }
+        inline uint8_t size() const {
+            return count;
+        }
+        inline const char *item(uint8_t idx) const {
+            return items[idx];
+        }
 };
